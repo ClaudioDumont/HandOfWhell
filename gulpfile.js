d0dfab59b3c1912015f48cae8be9aa4a8a5ae8d0
ClaudioDumont <claudio@chaordicsystems.com>
2016-07-20 05:38:40 -0300

    First Commit: Build System, with gulp, jade and live reload

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp'),
+	jade = require('gulp-jade'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	browserSync = require('browser-sync').create();
+
+
+gulp.task('browser-sync', function(){
+	browserSync.init({
+		server: {
+			baseDir: 'builds/development'
+		},
+	})
+});
+
+gulp.task('jade', function(){
+	return gulp.src('src/templates/**/*.jade')
+	.pipe(jade())
+	.pipe(gulp.dest('builds/development'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+});
+
+gulp.task('sass', function(){
+	return gulp.src('src/assets/sass/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('builds/development/assets/css'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+});
+
+gulp.task('scripts', function(){
+	gulp.src('src/assets/js/**/*.js')
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('builds/development/assets/js'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+});
+
+gulp.task('watch', ['sass', 'jade', 'browser-sync'], function(){
+	gulp.watch('src/assets/sass/**/*.scss', ['sass']);
+	gulp.watch('src/templates/**/*.jade', ['jade']);
+	gulp.watch('src/assets/js/**/*.js', ['scripts']);
+});
